93e7480c889c9b35e1d2f2c7f17c41c341d5d8d8
Corey Johnson <probablycorey@gmail.com>
2019-10-28 13:23:51 -0700

    🌭

--- main.c
+++ main.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
-#include "lua.h"
-#include <lauxlib.h>
-#include <lualib.h>
 
 int lua_main(char* script) {
 	printf("🤢 %s\n", script);
-	lua_State* lua = luaL_newstate();
-	luaL_openlibs(lua);
-	int res = luaL_dostring(lua, script);
-	size_t len = 0;
-	const char* value = lua_tolstring(lua, lua_gettop(lua), &len);
-
-	if (res == 0) {
-		printf("🚀 %s %s\n", value, script);
-	} else {
-		printf("🛑 %s\n", value);
-	}
-
-	lua_close(lua);
-
-	return res;
-}
-
-int main(int argc, char *argv[]) {
-	return lua_main("return 4");
+	return 1;
 }
